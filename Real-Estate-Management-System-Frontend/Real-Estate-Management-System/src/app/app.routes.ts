408d59ec977f6e88753bc6ca1038529fad7c64d2
Shpetim10 <shabanajshpetim@gmail.com>
2025-04-30 01:46:15 +0200

    Added log in form

--- Real-Estate-Management-System-Frontend/Real-Estate-Management-System/src/app/app.routes.ts
+++ Real-Estate-Management-System-Frontend/Real-Estate-Management-System/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AddPropertyComponent } from './add-property/add-property.component';
 import { PropertyManagementComponent } from './property-management/property-management.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 export const routes: Routes = [
     {path: "add-property", component: AddPropertyComponent},
     {path: "property-management", component: PropertyManagementComponent},
-    {path: "add-user",component: AddUserComponent}
+    {path: "add-user",component: AddUserComponent},
+    {path: "log-in", component: LogInComponent}
 ];
