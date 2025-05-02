647c84d0ce6f675150ae2a683fe5b7fd88e9ea87
Michele Celkavaja <michele.celkavaja21@gmail.com>
2025-05-02 23:09:09 +0200

    add user component

--- Real-Estate-Management-System-Backend/Real-Estate-Management-System-Backend/src/main/java/com/RealEstate/Real_Estate_Management_System_Backend/service/UserService.java
+++ Real-Estate-Management-System-Backend/Real-Estate-Management-System-Backend/src/main/java/com/RealEstate/Real_Estate_Management_System_Backend/service/UserService.java
@@ -6,8 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -31,7 +29,6 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    @Transactional
     public User updateUser(long id, User updatedUser) {
         logger.info("Updating user with ID: {}", id);
 
