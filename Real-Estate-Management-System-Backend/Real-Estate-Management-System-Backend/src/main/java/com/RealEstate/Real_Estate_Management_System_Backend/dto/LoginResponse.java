666add99654e083583a428a870893f6ea1a8427e
Michele Celkavaja <michele.celkavaja21@gmail.com>
2025-05-02 23:18:53 +0200

    add user component

--- Real-Estate-Management-System-Backend/Real-Estate-Management-System-Backend/src/main/java/com/RealEstate/Real_Estate_Management_System_Backend/dto/LoginResponse.java
+++ Real-Estate-Management-System-Backend/Real-Estate-Management-System-Backend/src/main/java/com/RealEstate/Real_Estate_Management_System_Backend/dto/LoginResponse.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponse {
-    private String message;
+    private String token;
+    private String username;
+    private List<String> roles;
     private boolean success;
+    private String message;
 }
